5b30236b7161551b15d0e3e3824fbc0fd730dcb0
Heye Voecking <heye.voecking@gmail.com>
2025-07-14 13:35:07 +0200

    fix: fix AutoAdvanceFlow test

--- cypress/e2e/ui/flows/AutoAdvanceFlow.cy.ts
+++ cypress/e2e/ui/flows/AutoAdvanceFlow.cy.ts
@@ -3,7 +3,7 @@ import { setupDefaultActions } from "../../utils";
 context("AutoAdvance Flow", () => {
   setupDefaultActions();
 
-  it("should pause on toggle switch to auto advance off", () => {
+  it("should not pause on toggle switch to auto advance off", () => {
     // Given the auto advance mode is on
     cy.get(".pause-icon").should("be.visible");
     cy.get("[data-test-id='speed-medium-icon']").should("have.class", "opacity-100");
@@ -12,6 +12,6 @@ context("AutoAdvance Flow", () => {
     cy.get("[data-test-id='speed-medium-icon']").click();
     cy.get("[data-test-id='speed-medium-icon']").should("have.class", "opacity-50");
 
-    cy.get(".play-icon").should("be.visible");
+    cy.get(".pause-icon").should("be.visible");
   });
 });
